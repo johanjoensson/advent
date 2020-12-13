b3f503ac21b3c8c0f61043b089c009db1fe0ac30
Johan Jönsson <johanjoensson@gmail.se>
2020-12-13 21:21:56 +0100

    Fuck it.

--- 2020/day_10/connect.hs
+++ 2020/day_10/connect.hs
@@ -1,5 +1,7 @@
 import System.IO
 import Data.List (sort, nub)
+import Data.Map (Map, insert, empty, (!), member)
+import Control.Monad.Fix
 
 get_adapters :: [String] -> [Int]
 get_adapters [] = []
@@ -16,34 +18,39 @@ count_occurences a b (x:rest)
 			    | otherwise = (a_occ, b_occ)
 			    where (a_occ, b_occ) = count_occurences a b rest
 
-check_valid :: Int -> Int -> [Int] -> Bool
-check_valid _ _ [] =  False
-check_valid start stop list
-			   | head list > start + 3 = False
-			   | otherwise = all (<= 3) $ get_differences $ (list)
-			    
+connect_to :: Map Int [Int] -> [Int] -> Map Int [Int]
+connect_to connections [] = connections
+connect_to connections (joltage:rest) = connect_to new_connections rest
+			    where new_connections = insert joltage connectors connections
+				  connectors  = [i | i <- [joltage -3 .. joltage - 1], i `member` connections]
 
---add_if_valid :: Int -> Int -> Int -> [Int] -> [Int]
-add_if_valid max_val _ x [] 
-			  | max_val - x <= 3 = x:[]
-			  | otherwise = []
-add_if_valid _ max_diff x sublist
-		      | (head sublist) - x <= max_diff = x:sublist
-		      | otherwise = sublist
+ways_to :: Map Int [Int] -> Int -> Int
+ways_to connections joltage
+			  | connectors == [] = 1
+			  | otherwise = (sum . map (ways_to connections) $ connectors)
+			  where connectors = connections ! joltage
+				next_joltage = head connectors
 
-all_possibilities :: Int -> [Int] -> [[Int]]
-all_possibilities _ [] = [[]]
-all_possibilities max_val (x:rest) = nub $ sublist ++ (map (add_if_valid max_val 3 x) sublist)
-			   where sublist = all_possibilities max_val rest
+memo_ways_to f (connections, joltage)
+				| connectors == [] = 1
+			  	| otherwise = sum . map (\i -> f (connections, i)) $ connectors
+			  	where connectors = connections ! joltage
+
+
+count_blocks :: Int -> [Int] -> [Int]
+count_blocks n [] = n:[]
+count_blocks n (x:rest)
+		    | x `mod` 3 == 1 = count_blocks (n + 1) rest
+		    | n > 0 = n : (count_blocks 0 rest)
+		    | otherwise = count_blocks 0 rest
 
 main = do
 	content <- readFile "adapters.txt"
 	putStrLn "Number of adapters in bag"
 	print . length . get_adapters . lines $ content
 	let (ones, threes) = count_occurences [1] [3] . get_differences . sort . get_adapters . lines $ content
-	putStrLn "Number of differences of 1 jolt multiplied by number of differences of 3 jolt"
-	print  $ (ones + 1) * (threes + 1)
-	putStrLn "All valid configurations"
+	putStrLn "Number of valid connections"
 	let adapters = sort . get_adapters . lines $ content
-	let end_val = (maximum adapters) + 3
-	print . length $ [l | l <-  nub . all_possibilities end_val $ adapters, check_valid 0 end_val l]
+	let max_val = 3 + maximum adapters
+	let connections = connect_to empty (0:adapters++[max_val])
+	print $ fix memo_ways_to (connections, max_val)
