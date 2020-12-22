6d6b20a4287c097c2f1c7b1e573ac9b842b71294
Johan Jönsson <johanjoensson@gmail.se>
2020-12-22 22:06:14 +0100

    Memory game done!

--- 2020/day_15/memory_game.hs
+++ 2020/day_15/memory_game.hs
@@ -17,10 +17,21 @@ insert_init memory _ [] = memory
 insert_init memory round (num:rest) = insert_init new_memory (round + 1) rest
     where new_memory = insert num round memory
 
+get_number :: [Int] -> Int -> Int
+get_number init n
+                | n < (length init) = init !! n
+                | otherwise = go (insert_init empty 0 init) (length init) (last init)
+                where go memory round previous
+                                    | round == n = new_val
+                                    | otherwise = go new_memory (round + 1) new_val
+                                    where (new_val, new_memory) = next_number memory (round - 1, previous)
 main = do
     contents <- readFile "starting.txt"
     let initials = map (\i -> read i ::Int) . splitOn "," . head . filter ((0 <) . length) . lines $ contents
     putStrLn "Read this number of initial numbers"
     print . length $ initials
     let start = insert_init empty 0 initials
-    putStrLn "Done"
+    putStrLn "2020th number in the sequence is"
+    print . get_number initials $ 2019
+    putStrLn "30000000th number in the sequence is"
+    print . get_number initials $ 29999999
